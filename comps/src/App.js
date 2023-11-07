570fa39cc19a5fc0403ee90ceb25dbbc392d0210
fcolnoz9 <fcolnoz9@gmail.com>
2023-11-07 14:14:44 -0300

    fix(button.jsx): fix error

---
+++ comps/src/App.js
@@ -0,0 +1,26 @@
+import Button from './Button';
+
+function App() {
+	return (
+		<div>
+			<div>
+				<Button primary>Primary button</Button>
+			</div>
+			<div>
+				<Button secondary>Secundary button</Button>
+			</div>
+			<div>
+				<Button ></Button>
+			</div>
+			<div>
+				<Button>Hola 4</Button>
+			</div>
+			<div>
+				<Button>Hola 4</Button>
+			</div>
+		</div>
+
+	);
+}
+
+export default App;
