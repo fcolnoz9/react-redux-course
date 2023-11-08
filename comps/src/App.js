b157c941000490fb861109d812b802de10138ab2
fcolnoz9 <fcolnoz9@gmail.com>
2023-11-08 10:48:24 -0300

    refactor(buttonpage): buttonPage refactor

--- comps/src/App.js
+++ comps/src/App.js
@@ -3,16 +3,19 @@ import Accordion from './components/Accordion';
 function App() {
   const items = [
     {
+      id: 'asdasdasda',
       label: 'Can I use React on a project?',
       content:
         'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
     },
     {
+      id: 'qweqweqwe',
       label: 'Can I use Javascript on a project?',
       content:
         'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
     },
     {
+      id: 'qweqweqwe1231',
       label: 'Can I use CSS on a project?',
       content:
         'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
--- comps/src/pages/ButtonPage.js
+++ comps/src/pages/ButtonPage.js
@@ -1,49 +1,61 @@
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from '../components/Button';
+import className from 'classnames';
 
-function ButtonPage() {
-  const handleClick = () => {
-    console.log('Clicked!');
-  };
+function Button({
+                    children,
+                    primary,
+                    secondary,
+                    success,
+                    warning,
+                    danger,
+                    outline,
+                    rounded,
+                    ...rest
+                }) {
+    /**
+     * @link https://www.npmjs.com/package/classnames
+     * @type {string}
+     */
+    const classes = className(
+        rest.className,
+        'flex items-center px-3 py-1.5 border',
+        {
+            'border-blue-500 bg-blue-500 text-white': primary,
+            'border-gray-900 bg-gray-900 text-white': secondary,
+            'border-green-500 bg-green-500 text-white': success,
+            'border-yellow-400 bg-yellow-400 text-white': warning,
+            'border-red-500 bg-red-500 text-white': danger,
+            'rounded-full': rounded,
+            'bg-white': outline,
+            'text-blue-500': outline && primary,
+            'text-gray-900': outline && secondary,
+            'text-green-500': outline && success,
+            'text-yellow-400': outline && warning,
+            'text-red-500': outline && danger,
+        }
+    );
 
-  return (
-    <div>
-      <div>
-        <Button
-          secondary
-          outline
-          rounded
-          className="mb-5"
-          onClick={handleClick}
-        >
-          <GoBell />
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline onMouseEnter={handleClick}>
-          <GoCloudDownload />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning onMouseLeave={handleClick}>
-          <GoDatabase />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-    </div>
-  );
+    return (
+        <button {...rest} className={classes}>
+            {children}
+        </button>
+    );
 }
 
-export default ButtonPage;
+Button.propTypes = {
+    checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+        const count =
+            Number(!!primary) +
+            Number(!!secondary) +
+            Number(!!warning) +
+            Number(!!success) +
+            Number(!!danger);
+
+        if (count > 1) {
+            return new Error(
+                'Only one of primary, secondary, success, warning, danger can be true'
+            );
+        }
+    },
+};
+
+export default Button;
