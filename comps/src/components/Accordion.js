5a4b9d474555134bd0c2e9378e6c0c72f1766e51
fcolnoz9 <fcolnoz9@gmail.com>
2023-11-08 16:14:48 -0300

    refactor(accordion): added styles and improve event onClick

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,24 +1,33 @@
 import {useState} from 'react';
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({items}) {
 
 	const [expandedIndex, setExpandedIndex] = useState(-1);
 
 	const handleOnClick = (nextIndex) => {
-		setExpandedIndex(nextIndex);
+		setExpandedIndex((currentIndexExpanded) => {
+			return (currentIndexExpanded === nextIndex) ? -1 : nextIndex;
+		});
 	};
 
 	const renderedItems = items.map((item, index) => {
 		const isExpanded = (index === expandedIndex);
+		const icon = <span className="text-2xl">
+			{ isExpanded ? <GoChevronDown/>: <GoChevronLeft/>}
+		</span>
 		return (
 			<div key={item.id}>
-				<div onClick={() => handleOnClick(index)}>{item.label}</div>
-				{isExpanded && <div>{item.content}</div>}
+				<div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleOnClick(index)}>
+					{item.label}
+					{icon}
+				</div>
+				{isExpanded && <div className="border-b p-5">{item.content}</div>}
 			</div>
 		);
 	});
 
-	return <div>{renderedItems}</div>;
+	return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
